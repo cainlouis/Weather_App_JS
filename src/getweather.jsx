9c76d4096a76b7227b44ac5261ce89ff4858546f
Nael Louis <nael.louis1002@gmail.com>
2020-12-14 00:31:50 -0500

    Modifications made to getweather

--- src/getweather.jsx
+++ src/getweather.jsx
@@ -1,6 +1,11 @@
 'use strict'
 
+let global = {};
+
 function setup() {
+    global.url = new URL('http://api.openweathermap.org/data/2.5/weather');
+    global.urlIcon = 'http://openweathermap.org/img/wn/';
+    global.appid = 'appid=' + 'a67203050719f4d6013a272df6ac734b';
     ReactDOM.render(<GetWeather />, document.querySelector('form'));
 }
 
@@ -9,6 +14,19 @@ class GetWeather extends React.Component {
         super(props);
         this.state = {
         }
+        this.city = '';
+        this.iconUrl = '';
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    change(e) {
+        this.city = e.currentTarget.value;
+    }
+
+    submit(e) {
+        e.preventDefault();
+        
+        
     }
 }
 
